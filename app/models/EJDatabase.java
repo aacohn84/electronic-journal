ed87d835b7364c24ccfc38291474afd0fa09bbff
Aaron Cohn <aacohn84@gmail.com>
2013-11-12 20:20:43 -0800

    Moved database code from stored procedures into app

--- app/models/EJDatabase.java
+++ app/models/EJDatabase.java
@@ -1,6 +1,6 @@
 package models;
 
-import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -13,6 +13,127 @@ import util.Pair;
 public class EJDatabase {
 
 	/**
+	 * Gets a list of groups in which the specified user is a member. Group
+	 * membership corresponds to an entry in the roster table.
+	 * 
+	 * @param userId
+	 *            - Unique ID of the user for whom to retrieve a list of groups.
+	 * @return A list of groups if successful, otherwise an empty list. Never
+	 *         <code>null</code>.
+	 */
+	public static ArrayList<Group> getGroups(int userId) {
+		final String getGroupsQuery = 
+				"SELECT * \r\n" + 
+				"FROM `roster` LEFT JOIN `group`\r\n" + 
+				"USING(`id_group`)\r\n" + 
+				"WHERE `id_user` = ?;";
+	
+		ArrayList<Group> groups = new ArrayList<Group>();
+		
+		try (Connection c = DB.getConnection();
+				PreparedStatement getGroupsStmt = c.prepareStatement(getGroupsQuery);) {
+	
+			getGroupsStmt.setInt(1, userId);
+	
+			ResultSet rs = getGroupsStmt.executeQuery();
+	
+			// build list of groups from result set
+			while (rs.next()) {
+				Group g = new Group(rs);
+				groups.add(g);
+			}
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return groups;
+	}
+
+	public static ArrayList<Group> getGroupsTaughtByUser(int teacherId) {
+		final String getGroupsTaughtByUserCall = "SELECT * FROM `group` WHERE `id_teacher` = ?;";
+	
+		ArrayList<Group> groups = new ArrayList<Group>();
+	
+		try (Connection c = DB.getConnection();
+				PreparedStatement getGroupsTaughtByUserStmt = c
+						.prepareStatement(getGroupsTaughtByUserCall);) {
+	
+			getGroupsTaughtByUserStmt.setInt(1, teacherId);
+	
+			ResultSet rs = getGroupsTaughtByUserStmt.executeQuery();
+	
+			while (rs.next()) {
+				Group g = new Group(rs);
+				groups.add(g);
+			}
+	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+		return groups;
+	}
+
+	/**
+	 * Gets the most recent prompt for the group specified by
+	 * <code>groupId</code>. If the user specified by <code>responderId</code>
+	 * has responded to the prompt, then the response will also be retrieved.
+	 * 
+	 * @param groupId
+	 *            - Unique ID of the group for which the most recent prompt will
+	 *            be retrieved.
+	 * @param responderId
+	 *            - Unique ID of a user in the group specified by
+	 *            <code>groupId</code>. If this user has responded to the most
+	 *            recent prompt, their response will be fetched.
+	 * @return a <code>Pair</code> containing <code>Prompt</code> and
+	 *         <code>Response</code>. Returns <code>null</code> if no results
+	 *         were found. <code>Response</code> is <code>null</code> if no
+	 *         response exists.
+	 */
+	public static Pair<Prompt, Response> getMostRecentPromptAndResponse(int groupId,
+			int responderId) {
+	
+		final String getMostRecentPromptAndResponseQuery = 
+				"SELECT * \r\n" + 
+				"FROM `prompt` LEFT JOIN (\r\n" + 
+				"	SELECT * FROM `prompt_response` WHERE `id_responder` = ?\r\n" + 
+				") `prompt_response`\r\n" + 
+				"USING(`id_prompt`)\r\n" + 
+				"WHERE `id_group` = ?\r\n" + 
+				"ORDER BY `prompt`.`creation_date` DESC\r\n" + 
+				"LIMIT 1;";
+	
+		Pair<Prompt, Response> pair = null;
+		
+		try (Connection c = DB.getConnection();
+				PreparedStatement getMostRecentPromptAndResponseStmt = c
+						.prepareStatement(getMostRecentPromptAndResponseQuery);) {
+	
+			getMostRecentPromptAndResponseStmt.setInt(1, responderId);
+			getMostRecentPromptAndResponseStmt.setInt(2, groupId);
+	
+			ResultSet rs = getMostRecentPromptAndResponseStmt.executeQuery();
+	
+			if (rs.first()) {
+				// a prompt is guaranteed to exist
+				pair = new Pair<Prompt, Response>();
+				pair.setA(new Prompt(rs));
+				// response not guaranteed, so we check if response text is null
+				if (rs.getString("prompt_response.text") != null) {
+					pair.setB(new Response(rs));
+				}
+			}
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+		return pair;
+	}
+
+	/**
 	 * Gets the <code>User</code> record associated with the given
 	 * <code>userId</code>.
 	 * 
@@ -22,10 +143,11 @@ public class EJDatabase {
 	 *         <code>null</code>.
 	 */
 	public static User getUser(int userId) {
-		final String getUserCall = "{call GET_USER_BY_ID(?)}";
+		final String getUserQuery = 
+				"SELECT * FROM `user` WHERE `id_user` = ?;";
 		User user = null;
 		try (Connection c = DB.getConnection();
-				CallableStatement getUserStmt = c.prepareCall(getUserCall)) {
+				PreparedStatement getUserStmt = c.prepareStatement(getUserQuery)) {
 			getUserStmt.setInt(1, userId);
 			ResultSet rs = getUserStmt.executeQuery();
 			if (rs.first()) {
@@ -47,10 +169,10 @@ public class EJDatabase {
 	 *         <code>null</code>.
 	 */
 	public static User getUser(String username) {
-		final String getUserCall = "{call GET_USER_BY_NAME(?)}";
+		final String getUserQuery = "SELECT * FROM `user` WHERE `unique_name` = ?;";
 		User user = null;
 		try (Connection c = DB.getConnection();
-				CallableStatement getUserStmt = c.prepareCall(getUserCall)) {
+				PreparedStatement getUserStmt = c.prepareStatement(getUserQuery)) {
 			getUserStmt.setString(1, username);
 			ResultSet rs = getUserStmt.executeQuery();
 			if (rs.first()) {
@@ -75,20 +197,77 @@ public class EJDatabase {
 	 *         <code>null</code>.
 	 */
 	public static User getUser(String username, String password) {
-		final String getUserCall = "{call GET_USER_BY_NAME_AND_PASSWORD(?,?)}";
+		final String getUserQuery = 
+				"SELECT * FROM `user` WHERE `unique_name` = ? AND `password` = ?;";
 		User user = null;
 		try (Connection c = DB.getConnection();
-				CallableStatement getUserStmt = c.prepareCall(getUserCall)) {
+				PreparedStatement getUserStmt = c.prepareStatement(getUserQuery)) {
 			getUserStmt.setString(1, username);
 			getUserStmt.setString(2, password);
 			ResultSet rs = getUserStmt.executeQuery();
 			if (rs.first()) {
 				user = new User(rs);
 			}
+			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return user;
+	}
+
+	public static boolean isTeacher(int userId) {
+		final String isTeacherQuery = "SELECT * FROM `teacher` WHERE `id_teacher` = ?";
+		
+		boolean isTeacher = false;
+		try (Connection c = DB.getConnection();
+				PreparedStatement isTeacherStmt = c.prepareStatement(isTeacherQuery);) {
+	
+			isTeacherStmt.setInt(1, userId);
+	
+			ResultSet rs = isTeacherStmt.executeQuery();
+			
+			isTeacher = rs.first(); // true if a row was returned
+			
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return isTeacher;
+	}
+
+	public static void saveResponse(int responderId, int promptId,
+			String responseText) throws EJDatabaseException {
+	
+		final String saveResponseQuery = 
+				"INSERT INTO `prompt_response` (\r\n" + 
+				"	`id_responder`,\r\n" + 
+				"	`id_prompt`,\r\n" + 
+				"	`text`,\r\n" + 
+				"	`creation_date`\r\n" + 
+				") VALUES (?, ?, ?, NOW())\r\n" +
+				"ON DUPLICATE KEY UPDATE `text` = values(`text`);";
+	
+		try (Connection c = DB.getConnection();
+				PreparedStatement saveResponseStmt = c
+						.prepareStatement(saveResponseQuery);) {
+	
+			saveResponseStmt.setInt(1, responderId);
+			saveResponseStmt.setInt(2, promptId);
+			saveResponseStmt.setString(3, responseText);
+	
+			int numRowsUpdated = saveResponseStmt.executeUpdate();
+			if (numRowsUpdated == 0) {
+				throw new EJDatabaseException(
+						"Problem saving response to database"
+								+ "\nresponder_id: " + responderId
+								+ "\nprompt_id: " + promptId + "\ntext: "
+								+ responseText);
+			}
+	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -105,11 +284,16 @@ public class EJDatabase {
 	public static void writePrompt(int groupId, String promptText)
 			throws EJDatabaseException {
 
-		final String writePromptCall = "{call WRITE_PROMPT(?,?)}";
+		final String writePromptQuery = 
+				"INSERT INTO `prompt` (\r\n" + 
+				"	`id_group`,\r\n" + 
+				"	`text`,\r\n" + 
+				"	`creation_date`\r\n" + 
+				") VALUES (?, ?, NOW());";
 
 		try (Connection c = DB.getConnection();
-				CallableStatement writePromptStmt = c
-						.prepareCall(writePromptCall);) {
+				PreparedStatement writePromptStmt = c
+						.prepareStatement(writePromptQuery);) {
 
 			writePromptStmt.setInt(1, groupId);
 			writePromptStmt.setString(2, promptText);
@@ -127,138 +311,7 @@ public class EJDatabase {
 		}
 	}
 
-	public static void saveResponse(int responderId, int promptId,
-			String responseText) throws EJDatabaseException {
-
-		final String saveResponseCall = "{call SAVE_RESPONSE(?,?,?)}";
-
-		try (Connection c = DB.getConnection();
-				CallableStatement saveResponseStmt = c
-						.prepareCall(saveResponseCall);) {
-
-			saveResponseStmt.setInt(1, responderId);
-			saveResponseStmt.setInt(2, promptId);
-			saveResponseStmt.setString(3, responseText);
-
-			int numRowsUpdated = saveResponseStmt.executeUpdate();
-			if (numRowsUpdated == 0) {
-				throw new EJDatabaseException(
-						"Problem saving response to database"
-								+ "\nresponder_id: " + responderId
-								+ "\nprompt_id: " + promptId + "\ntext: "
-								+ responseText);
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Gets the most recent prompt for the group specified by
-	 * <code>groupId</code>. If the user specified by <code>responderId</code>
-	 * has responded to the prompt, then the response will also be retrieved.
-	 * 
-	 * @param groupId
-	 *            - Unique ID of the group for which the most recent prompt will
-	 *            be retrieved.
-	 * @param responderId
-	 *            - Unique ID of a user in the group specified by
-	 *            <code>groupId</code>. If this user has responded to the most
-	 *            recent prompt, their response will be fetched.
-	 * @return a <code>Pair</code> containing <code>Prompt</code> and
-	 *         <code>Response</code>. Returns <code>null</code> if no results
-	 *         were found. <code>Response</code> is <code>null</code> if no
-	 *         response exists.
-	 */
-	public static Pair<Prompt, Response> getMostRecentPromptAndResponse(int groupId,
-			int responderId) {
-
-		final String getMostRecentPromptAndResponseCall = "{call GET_MOST_RECENT_PROMPT_AND_RESPONSE(?,?)}";
-
-		Pair<Prompt, Response> p = null;
-		
-		try (Connection c = DB.getConnection();
-				CallableStatement getMostRecentPromptAndResponseStmt = c
-						.prepareCall(getMostRecentPromptAndResponseCall);) {
-
-			getMostRecentPromptAndResponseStmt.setInt(1, groupId);
-			getMostRecentPromptAndResponseStmt.setInt(2, responderId);
-
-			ResultSet rs = getMostRecentPromptAndResponseStmt.executeQuery();
-
-			if (rs.first()) {
-				printResultSetColumnNames(rs);
-				p = new Pair<Prompt, Response>();
-				p.setA(new Prompt(rs));
-				if (rs.getString("prompt_response.text") != null) {
-					p.setB(new Response(rs));
-				}
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return p;
-	}
-	
-	/**
-	 * Gets a list of groups in which the specified user is a member. Group
-	 * membership corresponds to an entry in the roster table.
-	 * 
-	 * @param userId
-	 *            - Unique ID of the user for whom to retrieve a list of groups.
-	 * @return A list of groups if successful, otherwise an empty list. Never
-	 *         <code>null</code>.
-	 */
-	public static ArrayList<Group> getGroups(int userId) {
-		final String getGroupsCall = "{call GET_GROUPS(?)}";
-
-		ArrayList<Group> groups = new ArrayList<Group>();
-		
-		try (Connection c = DB.getConnection();
-				CallableStatement getGroupsStmt = c.prepareCall(getGroupsCall);) {
-
-			getGroupsStmt.setInt(1, userId);
-
-			ResultSet rs = getGroupsStmt.executeQuery();
-
-			while (rs.next()) {
-				Group g = new Group(rs);
-				groups.add(g);
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return groups;
-	}
-	
-	public static boolean isTeacher(int userId) {
-		// TODO: Fix the stored procedure so it doesn't cause an exception when the user is not a teacher
-		final String isTeacherCall = "{call IS_TEACHER(?,?)}";
-		
-		boolean isTeacher = false;
-		try (Connection c = DB.getConnection();
-				CallableStatement isTeacherStmt = c.prepareCall(isTeacherCall);) {
-
-			isTeacherStmt.setInt(1, userId);
-			isTeacherStmt.registerOutParameter(2, java.sql.Types.BOOLEAN);
-
-			isTeacherStmt.execute();
-			
-			// capture the output of the stored procedure call
-			isTeacher = isTeacherStmt.getBoolean(2);
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return isTeacher;
-	}
-	
+	@SuppressWarnings("unused")
 	private static void printResultSetColumnNames(ResultSet rs) throws SQLException {
 		ResultSetMetaData metaData = rs.getMetaData();
 		int columnCount = metaData.getColumnCount();
@@ -266,30 +319,5 @@ public class EJDatabase {
 			System.out.print(metaData.getColumnName(i) + ", ");
 		}
 		System.out.print(metaData.getColumnName(columnCount) + "\n");
-	}
-
-	public static ArrayList<Group> getGroupsTaughtByUser(int teacherId) {
-		final String getGroupsTaughtByUserCall = "{call GET_GROUPS_TAUGHT_BY_USER(?)}";
-
-		ArrayList<Group> groups = new ArrayList<Group>();
-
-		try (Connection c = DB.getConnection();
-				CallableStatement getGroupsTaughtByUserStmt = c
-						.prepareCall(getGroupsTaughtByUserCall);) {
-
-			getGroupsTaughtByUserStmt.setInt(1, teacherId);
-
-			ResultSet rs = getGroupsTaughtByUserStmt.executeQuery();
-
-			while (rs.next()) {
-				Group g = new Group(rs);
-				groups.add(g);
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return groups;
 	}
 }
